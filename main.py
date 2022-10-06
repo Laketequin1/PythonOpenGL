2d200b13f359d5a12f798ff887b32abe29ca6ca8
Laketequin1 <laketequin1@gmail.com>
2022-10-06 21:01:23 +1300

    Camera and key movement!

--- main.py
+++ main.py
@@ -14,56 +14,248 @@ class Cube:
         self.position = np.array(position, dtype=np.float32)
 
 
+class Player:
+
+    def __init__(self, position):
+
+        self.position = np.array(position, dtype = np.float32)
+        self.theta = 0
+        self.phi = 0
+        self.update_vectors()
+    
+    def update_vectors(self):
+
+        self.forwards = np.array(
+            [
+                np.cos(np.deg2rad(self.theta)) * np.cos(np.deg2rad(self.phi)),
+                np.sin(np.deg2rad(self.phi)),
+                np.sin(np.deg2rad(self.theta)) * np.cos(np.deg2rad(self.phi))
+            ],
+            dtype = np.float32
+        )
+
+        global_up = np.array([0, 1, 0], dtype=np.float32)
+
+        self.right = np.cross(self.forwards, global_up)
+
+        self.up = np.cross(self.right, self.forwards)
+
+
+class Scene:
+
+    def __init__(self):
+
+        self.cubes = [
+            Cube(
+                position = [6, 0, 0],
+                eulers = [0, 0, 0]
+            ),
+        ]
+
+        self.player = Player(
+            position = [0, 2, 0]
+        )
+
+    def update(self, rate):
+
+        for cube in self.cubes:
+            cube.eulers[1] += 0.25 * rate
+            if cube.eulers[1] > 360:
+                cube.eulers[1] -= 360
+
+    def move_player(self, d_pos):
+
+        d_pos = np.array(d_pos, dtype = np.float32)
+        self.player.position += d_pos
+    
+    def spin_player(self, d_theta, d_phi):
+
+        self.player.theta += d_theta
+        if self.player.theta > 360:
+            self.player.theta -= 360
+        elif self.player.theta < 0:
+            self.player.theta += 360
+        
+        self.player.phi = min(
+            89, max(-89, self.player.phi + d_phi)
+        )
+        
+        self.player.update_vectors()
+
+
 class App:
     
-    def __init__(self):
+    def __init__(self, screen_width, screen_height):
+        
+        # Set variables
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        
+        self.renderer = GraphicsEngine(screen_width, screen_height)
+        
+        self.scene = Scene()
+        
+        self.last_time = pg.time.get_ticks()
+        self.current_time = 0
+        self.num_frames = 0
+        self.frame_time = 0
+        
+        self.main_loop()
+    
+    def main_loop(self):
+        
+        running = True
+        while running:
+            # Check events
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_ESCAPE:
+                        running = False
+            
+            self.handle_keys()
+            self.handle_mouse()
+            
+            self.scene.update(self.frame_time * 0.05)
+            self.renderer.render(self.scene)
+            
+            # Timing
+            self.calculate_framerate()
+            
+        self.quit()
+    
+    def handle_keys(self):
+
+        keys = pg.key.get_pressed()
+        combo = 0
+        direction_modifier = 0
+        
+        d_pos = [0, 0, 0]
+        
+        up = 0
+        down = 0
+        
+        """
+        w: 1 -> 0 degrees
+        a: 2 -> 90 degrees
+        w & a: 3 -> 45 degrees
+        s: 4 -> 180 degrees
+        w & s: 5 -> x
+        a & s: 6 -> 135 degrees
+        w & a & s: 7 -> 90 degrees
+        d: 8 -> 270 degrees
+        w & d: 9 -> 315 degrees
+        a & d: 10 -> x
+        w & a & d: 11 -> 0 degrees
+        s & d: 12 -> 225 degrees
+        w & s & d: 13 -> 270 degrees
+        a & s & d: 14 -> 180 degrees
+        w & a & s & d: 15 -> x
+        """
+
+        if keys[pg.K_w]:
+            combo += 1
+        if keys[pg.K_d]:
+            combo += 2
+        if keys[pg.K_s]:
+            combo += 4
+        if keys[pg.K_a]:
+            combo += 8
+        
+        if combo > 0:
+            if combo == 3:
+                direction_modifier = 45
+            elif combo == 2 or combo == 7:
+                direction_modifier = 90
+            elif combo == 6:
+                direction_modifier = 135
+            elif combo == 4 or combo == 14:
+                direction_modifier = 180
+            elif combo == 12:
+                direction_modifier = 225
+            elif combo == 8 or combo == 13:
+                direction_modifier = 270
+            elif combo == 9:
+                direction_modifier = 315
+            
+            d_pos[0] = self.frame_time * 0.02 * np.cos(np.deg2rad(self.scene.player.theta + direction_modifier))
+            d_pos[2] = self.frame_time * 0.02 * np.sin(np.deg2rad(self.scene.player.theta + direction_modifier))
+        
+        if keys[pg.K_SPACE]:
+            up = 1
+        if keys[pg.K_LCTRL]:
+            down = 1
+        
+        d_pos[1] = self.frame_time * 0.02 * (up - down)
+
+        self.scene.move_player(d_pos)
+    
+    def handle_mouse(self):
+
+        (x, y) = pg.mouse.get_pos()
+        theta_increment = self.frame_time * 0.05 * ((self.screen_width // 2) - x)
+        phi_increment = self.frame_time * 0.05 * ((self.screen_height // 2) - y)
+        self.scene.spin_player(-theta_increment, phi_increment)
+        pg.mouse.set_pos((self.screen_width // 2,self.screen_height // 2))
+
+    def calculate_framerate(self):
+
+        self.current_time = pg.time.get_ticks()
+        delta = self.current_time - self.last_time
+        if (delta >= 1000):
+            framerate = max(1,int(1000.0 * self.num_frames/delta))
+            pg.display.set_caption(f"Running at {framerate} fps.")
+            self.last_time = self.current_time
+            self.num_frames = -1
+            self.frame_time = float(1000.0 / max(1,framerate))
+        self.num_frames += 1
+
+    def quit(self):
+        
+        self.renderer.destroy()
+
+
+class GraphicsEngine:
+    
+    def __init__(self, screen_width, screen_height):
         
         # Initilize pygame
         pg.init()
-        pg.display.set_mode((640, 480), pg.OPENGL|pg.DOUBLEBUF)
-        self.clock = pg.time.Clock()
+        pg.mouse.set_visible(False)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,
+                                    pg.GL_CONTEXT_PROFILE_CORE)
+        pg.display.set_mode((screen_width, screen_height), pg.OPENGL|pg.DOUBLEBUF)
         
-        # Initilize OpenGL
-        gl.glClearColor(0.6, 0.6, 0.9, 1)
-        gl.glEnable(gl.GL_BLEND)
-        gl.glEnable(gl.GL_DEPTH_TEST)
-        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+        self.wood_texture = Material("gfx/wood.jpeg")
+        self.cube_mesh = Mesh("models/cube.obj")
+
+        #initialise opengl
+        gl.glClearColor(0.9, 0.5, 0.8, 1)
         self.shader = self.create_shader("shaders/vertex.txt", "shaders/fragment.txt")
         gl.glUseProgram(self.shader)
         gl.glUniform1i(gl.glGetUniformLocation(self.shader, "imageTexture"), 0)
-        
-        # Add cube and textures. Eulers - pitch, roll, yaw
-        self.cube = Cube(
-            position = [0, 0, -5],
-            eulers = [0, 0, 0]
-        )
-        
-        self.cube_mesh = Mesh('models/rendering_object.obj')
-        
-        self.wood_texture = Material("gfx/rendering_texture.jpg")
-        
-        # Generate perspective projection
+        gl.glEnable(gl.GL_DEPTH_TEST)
+
         projection_transform = pyrr.matrix44.create_perspective_projection(
-            fovy = 45, aspect = 640/480,
+            fovy = 45, aspect = 640/480, 
             near = 0.1, far = 50, dtype=np.float32
         )
-        
         gl.glUniformMatrix4fv(
-            gl.glGetUniformLocation(self.shader, "projection"),
+            gl.glGetUniformLocation(self.shader,"projection"),
             1, gl.GL_FALSE, projection_transform
         )
+        self.model_matrix_location = gl.glGetUniformLocation(self.shader, "model")
+        self.view_matrix_location = gl.glGetUniformLocation(self.shader, "view")
         
-        self.modelMatrixLocation = gl.glGetUniformLocation(self.shader, "model")
+    def create_shader(self, vertex_filepath, fragment_filepath):
         
-        # Start main
-        self.main_loop()
-        
-    def create_shader(self, vertexFilepath, fragmentFilepath):
-        
-        with open(vertexFilepath, 'r') as f:
+        with open(vertex_filepath, 'r') as f:
             vertex_src = f.readlines()
         
-        with open(fragmentFilepath, 'r') as f:
+        with open(fragment_filepath, 'r') as f:
             fragment_src = f.readlines()
         
         shader = gls.compileProgram(
@@ -73,70 +265,43 @@ class App:
         
         return shader
         
-    def main_loop(self):
-        
-        running = True
-        while running:
-            # Check events
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    running = False
-            
-            # Update
-            self.cube.eulers[2] += 0.8
-            if self.cube.eulers[2] > 360:
-                self.cube.eulers[2] = 0
-                
-            self.cube.eulers[1] += 0.4
-            if self.cube.eulers[1] > 360:
-                self.cube.eulers[1] = 0
-                
-            self.cube.eulers[0] += 0.1
-            if self.cube.eulers[0] > 360:
-                self.cube.eulers[0] = 0
-            
-            pg.display.set_caption(str(int(self.clock.get_fps())) + " fps")
-            
-            # Refresh screen
-            gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-            
-            # Draw
-            gl.glUseProgram(self.shader)
-            
-            self.wood_texture.use()
+    def render(self, scene):
+
+        #refresh screen
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        gl.glUseProgram(self.shader)
+
+        view_transform = pyrr.matrix44.create_look_at(
+            eye = scene.player.position,
+            target = scene.player.position + scene.player.forwards,
+            up = scene.player.up, dtype = np.float32
+        )
+        gl.glUniformMatrix4fv(self.view_matrix_location, 1, gl.GL_FALSE, view_transform)
+
+        for cube in scene.cubes:
+
             model_transform = pyrr.matrix44.create_identity(dtype=np.float32)
-            
-            # Eulers - pitch, roll, yaw
             model_transform = pyrr.matrix44.multiply(
-                m1 = model_transform,
-                m2 = pyrr.matrix44.create_from_eulers(
-                    eulers=np.radians(self.cube.eulers),
-                    dtype=np.float32
+                m1=model_transform, 
+                m2=pyrr.matrix44.create_from_eulers(
+                    eulers=np.radians(cube.eulers), dtype=np.float32
                 )
             )
-            
             model_transform = pyrr.matrix44.multiply(
-                m1 = model_transform,
-                m2 = pyrr.matrix44.create_from_translation(
-                    vec=self.cube.position,
-                    dtype=np.float32
+                m1=model_transform, 
+                m2=pyrr.matrix44.create_from_translation(
+                    vec=np.array(cube.position),dtype=np.float32
                 )
             )
-            
-            gl.glUniformMatrix4fv(self.modelMatrixLocation, 1, gl.GL_FALSE, model_transform)
+            gl.glUniformMatrix4fv(self.model_matrix_location, 1, gl.GL_FALSE, model_transform)
+            self.wood_texture.use()
             gl.glBindVertexArray(self.cube_mesh.vao)
             gl.glDrawArrays(gl.GL_TRIANGLES, 0, self.cube_mesh.vertex_count)
-            
+
             pg.display.flip()
-            
-            # Clock
-            self.clock.tick(120)
-            
-        self.quit()
     
-    def quit(self):
-        
-        # Remove allocated memory and quit pygame
+    def destroy(self):
+
         self.cube_mesh.destroy()
         self.wood_texture.destroy()
         gl.glDeleteProgram(self.shader)
@@ -260,4 +425,4 @@ class Material:
 
 
 if __name__ == "__main__":
-    myApp = App()
+    myApp = App(1200, 900)
